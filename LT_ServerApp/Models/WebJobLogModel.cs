4e26e2d599065e8fdcd4c2061b938dbb8d5b1a79
Shashikant Pund <shashi.pund86@gmail.com>
2022-01-09 21:28:43 +0530

    changed web job model

--- LT_ServerApp/Models/WebJobLogModel.cs
+++ LT_ServerApp/Models/WebJobLogModel.cs
@@ -14,6 +14,7 @@ namespace LT_ServerApp.Models
         public string EndDateTime { get; set; }
         public string Status { get; set; }
 
-        //New prop added
+        //New prop added  fhghgfhfgh gfhh
+        //fghfjfgff
     }
 }
